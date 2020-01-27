4c78568dafbbee44572737f755bfe44427e9c8c0
Constantine <wolverineo250kr@yandex.ru>
2020-01-27 09:37:00 +0500

    Update PevnevBlogGroupAsset.php

--- modules/frontend/assets/PevnevBlogGroupAsset.php
+++ modules/frontend/assets/PevnevBlogGroupAsset.php
@@ -12,16 +12,12 @@ class PevnevBlogGroupAsset extends AssetBundle
         'css/blog-index'.((YII_DEBUG) ? '' : '.min').'.css?v1.00',
 		'css/font-awesome/css/font-awesome.css',
     ];
-	    public $js        = [
+    public $js        = [
         'js/blog-index'.((YII_DEBUG) ? '' : '.min').'.js?v1.02',
     ];
-    public $cssOptions = [
-        'onload' => "if(media!='all') media='all'"
-    ];
     public $depends    = [
-	    'yii\web\YiiAsset',
-    'yii\bootstrap\BootstrapPluginAsset',
-	'yii\web\JqueryAsset'
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+        'yii\web\JqueryAsset'
     ];
-
 }
